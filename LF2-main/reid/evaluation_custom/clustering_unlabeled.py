e10b0e80296c50b44950b2042364cc96d31c8252
@dank1910 <53305399+ewigspace1910@users.noreply.github.com>
2022-06-28 10:52:50 +0700

    add review paper

--- LF2-main/reid/evaluation_custom/clustering_unlabeled.py
+++ LF2-main/reid/evaluation_custom/clustering_unlabeled.py
@@ -143,10 +143,11 @@ def clustering_all(distmat_gg, gallery=None, top_k = 15, label_clusters=None, ha
             
             num_j_accept_i = len(tmp_cp)
             if hardmore:
-                print("using hard samples")
+                print("using hard sample")
                 #i must in top k/2 of j => i (= cluster(j)  ~  significantly same reranking
-                distvec_j = indices[j]
-                if np.where(distvec_j == i)[0][0] > top_k // 2: num_j_accept_i-= 1             
+                for j in tmp_cp:
+                    distvec_j = indices[j]
+                    if np.where(distvec_j == i)[0][0] > top_k // 2: num_j_accept_i-= 1             
                        
             #print(rank_tmp_in_dv_i)
             constrain_1 = len(tmp_cp) // 5 * 4   #empritical belief :>>
---
+++ _reviewpaper_/notes/readme.md
@@ -0,0 +1,27 @@
+#Paper title
+This is template format
+* Official paper: [name](www.google.com)
+* Official code: [Github](www.google.com)
+
+##Overview
+
+1. main concept 1
+2. main concept 2
+
+##Main concept i
+
+###Core theory
+> theory's contents
+###Explain
+* sentence 1
+* code (maybe)
+```python
+print("hello world")
+```
+* image
+  ![image](../images/template.png)
+
+##Paper Experiment results
+
+1. Table 1
+2. Table 2
